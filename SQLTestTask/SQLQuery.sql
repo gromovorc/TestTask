cc0c0ca85da08c7cead84ff38d9b255602019e48
Arkady Gromov <arkady-gromov@mail.ru>
2024-02-24 01:12:56 +0300

    commit SQL
    Added SQL part

---
+++ SQLTestTask/SQLQuery.sql
@@ -0,0 +1,21 @@
+/*
+Предполагаю, что таблицы уже созданы, имеют структуру:
+dbo.Product (
+	ID int not null,
+	Name varchar(255) null
+	PK ID identity(1,1)
+)
+dbo.Category (
+	ID int not null,
+	Name varchar(255) null,
+	ID_Product int null
+	PK ID identity(1,1),
+	FK ID_Product dbo.Product ID
+)
+*\
+
+select
+	p.Name as ProductName
+	,c.Name as CategoryName
+from dbo.Product as p
+	left join dbo.Category as c on c.ID = p.ID_Product
